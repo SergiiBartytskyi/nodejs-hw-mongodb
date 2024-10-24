0765c3381e2a8ebda4805883947d7e7a10448f0c
SergiiBartytskyi <s.bartycjkyj@gmail.com>
2024-10-24 03:22:19 +0200

    add hw3-crud

---
+++ src/controllers/contacts.js
@@ -0,0 +1,130 @@
+import createHttpError from 'http-errors';
+import {
+  createContact,
+  deleteContact,
+  getAllContacts,
+  getContactById,
+  updateContact,
+} from '../services/contacts.js';
+
+export const getGreetingController = async (req, res) => {
+  res.json({
+    status: 200,
+    message: 'Hello in my Contacts DB!',
+  });
+};
+
+export const getContactsController = async (req, res) => {
+  const contacts = await getAllContacts();
+
+  res.json({
+    status: 200,
+    message: 'Successfully found contacts!',
+    data: contacts,
+  });
+};
+
+export const getContactByIdController = async (req, res, next) => {
+  const { contactId } = req.params;
+  const contact = await getContactById(contactId);
+
+  if (!contact) {
+    next(createHttpError(404, 'Contact not found!'));
+    return;
+  }
+
+  res.json({
+    status: 200,
+    message: `Successfully found contact with id ${contactId}!`,
+    data: contact,
+  });
+};
+
+export const createContactController = async (req, res, next) => {
+  const { name, phoneNumber, email, isFavourite, contactType } = req.body;
+
+  if (!name || !phoneNumber || !contactType) {
+    next(
+      createHttpError(400, 'Required fields: name, phoneNumber, contactType'),
+    );
+    return;
+  }
+
+  const newContact = await createContact({
+    name,
+    phoneNumber,
+    email,
+    isFavourite,
+    contactType,
+  });
+
+  res.status(201).json({
+    status: 201,
+    message: 'Successfully created a contact!',
+    data: newContact,
+  });
+};
+
+export const upsertContactController = async (req, res, next) => {
+  const { contactId } = req.params;
+  const { name, phoneNumber, contactType } = req.body;
+
+  const result = await updateContact(contactId, req.body, {
+    upsert: true,
+  });
+
+  if (!result) {
+    next(createHttpError(404, 'Contact not found!'));
+    return;
+  }
+
+  if (result.isNew) {
+    if (!name || !phoneNumber || !contactType) {
+      return next(
+        createHttpError(
+          400,
+          'Name, Phone Number, and Contact Type are required when creating a contact!',
+        ),
+      );
+    }
+  }
+
+  const status = result.isNew ? 201 : 200;
+
+  res.status(status).json({
+    status,
+    message: result.isNew
+      ? 'Successfully created a contact!'
+      : 'Successfully updated the contact!',
+    data: result.contact,
+  });
+};
+
+export const patchContactController = async (req, res, next) => {
+  const { contactId } = req.params;
+
+  const result = await updateContact(contactId, req.body);
+
+  if (!result) {
+    next(createHttpError(404, 'Contact not found!'));
+    return;
+  }
+
+  res.json({
+    status: 200,
+    message: `Successfully patched a contact!`,
+    data: result.contact,
+  });
+};
+
+export const deleteContactController = async (req, res, next) => {
+  const { contactId } = req.params;
+  const contact = await deleteContact(contactId);
+
+  if (!contact) {
+    next(createHttpError(404, 'Contact not found!'));
+    return;
+  }
+
+  res.status(204).send();
+};
