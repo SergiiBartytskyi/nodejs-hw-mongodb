24d249d67b75a872da0ecb98b1c642e3d31b20b9
SergiiBartytskyi <s.bartycjkyj@gmail.com>
2024-11-13 22:32:11 +0100

    refactoring

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -7,23 +7,20 @@ export const authenticate = async (req, res, next) => {
   const authHeader = req.get('Authorization');
 
   if (!authHeader) {
-    next(createHttpError(401, 'Please provide Authorization header!'));
-    return;
+    return next(createHttpError(401, 'Please provide Authorization header!'));
   }
 
   const bearer = authHeader.split(' ')[0];
   const token = authHeader.split(' ')[1];
 
   if (bearer !== 'Bearer' || !token) {
-    next(createHttpError(401, 'Auth header should be of type Bearer!'));
-    return;
+    return next(createHttpError(401, 'Auth header should be of type Bearer!'));
   }
 
   const session = await SessionsCollection.findOne({ accessToken: token });
 
   if (!session) {
-    next(createHttpError(401, 'Session not found!'));
-    return;
+    return next(createHttpError(401, 'Session not found!'));
   }
 
   const isAccessTokenExpired =
@@ -36,8 +33,7 @@ export const authenticate = async (req, res, next) => {
   const user = await UsersCollection.findById(session.userId);
 
   if (!user) {
-    next(createHttpError(401));
-    return;
+    return next(createHttpError(401, 'User not found!'));
   }
 
   req.user = user;
