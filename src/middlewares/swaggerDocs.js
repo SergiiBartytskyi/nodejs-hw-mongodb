57db3e7f1f6da506c0a0505c20b88f179f7eadbd
SergiiBartytskyi <s.bartycjkyj@gmail.com>
2024-11-24 23:22:10 +0100

    add route /api-docs

---
+++ src/middlewares/swaggerDocs.js
@@ -0,0 +1,21 @@
+import createHttpError from 'http-errors';
+import swaggerUI from 'swagger-ui-express';
+import fs from 'node:fs';
+
+import { SWAGGER_PATH } from '../constants/index.js';
+
+export const swaggerDocs = () => {
+  try {
+    const swaggerDoc = JSON.parse(fs.readFileSync(SWAGGER_PATH, 'utf8'));
+    return [...swaggerUI.serve, swaggerUI.setup(swaggerDoc)];
+  } catch (err) {
+    console.error('Error loading Swagger docs:', err.message);
+    return (req, res, next) =>
+      next(
+        createHttpError(
+          500,
+          `Failed to load Swagger documentation: ${err.message}`,
+        ),
+      );
+  }
+};
--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import router from './routers/index.js';
 import { env } from './utils/env.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
-import cookieParser from 'cookie-parser';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 import { UPLOAD_DIR } from './constants/index.js';
 
 const PORT = Number(env('PORT', '3000'));
@@ -27,6 +28,8 @@ export const setupServer = () => {
   app.use(router);
 
   app.use('/uploads', express.static(UPLOAD_DIR));
+
+  app.use('/api-docs', swaggerDocs());
 
   app.use('*', notFoundHandler);
 
